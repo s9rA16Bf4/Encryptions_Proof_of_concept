6309756537bf45e45696ea33675f0195456df0a6
Nicholas <nicholas.rosqvist.nunes@gmail.com>
2020-07-05 22:34:42 +0200

    Removed some comments

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from PIL import Image
 from random import randint
 import argparse
 
-# Required
-# PIL
-
 CTW = {}
 ALPHA = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ",
 "1","2","3","4","5","6","7","8","9","0", "!", "@", "£", "#", "$", "¤", "%","€","&","/","{","[","(",")","]","=","}","+","?","`","±", "A", "B", "C"
