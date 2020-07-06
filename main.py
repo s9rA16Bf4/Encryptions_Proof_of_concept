46aefcb1f02c76847d1f7b02fba66f9c4abbe523
Nicholas <nicholas.rosqvist.nunes@gmail.com>
2020-07-06 17:58:30 +0200

    Update 1.2.1
    Removed the main function and a bunch of unnecessary functions. I've 
    also added support to fill all blank pixels with random data, which are 
    randomly selected from the already generated pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from PIL import Image
-from random import randint
+from random import randint, choice
 from argparse import ArgumentParser
 from os import system
 
@@ -10,22 +10,10 @@ ALPHA = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "
 "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "\n", "\t",".", ",", ";", ">", "<", "|"
 , ":", '"']
 
-LOGO = """
- ▄████▄   ▒█████   ██▓     ▒█████   ██▀███     ▓█████  ███▄    █  ▄████▄   ██▀███ ▓██   ██▓ ██▓███  ▄▄▄█████▓ ██▓ ▒█████   ███▄    █
-▒██▀ ▀█  ▒██▒  ██▒▓██▒    ▒██▒  ██▒▓██ ▒ ██▒   ▓█   ▀  ██ ▀█   █ ▒██▀ ▀█  ▓██ ▒ ██▒▒██  ██▒▓██░  ██▒▓  ██▒ ▓▒▓██▒▒██▒  ██▒ ██ ▀█   █
-▒▓█    ▄ ▒██░  ██▒▒██░    ▒██░  ██▒▓██ ░▄█ ▒   ▒███   ▓██  ▀█ ██▒▒▓█    ▄ ▓██ ░▄█ ▒ ▒██ ██░▓██░ ██▓▒▒ ▓██░ ▒░▒██▒▒██░  ██▒▓██  ▀█ ██▒
-▒▓▓▄ ▄██▒▒██   ██░▒██░    ▒██   ██░▒██▀▀█▄     ▒▓█  ▄ ▓██▒  ▐▌██▒▒▓▓▄ ▄██▒▒██▀▀█▄   ░ ▐██▓░▒██▄█▓▒ ▒░ ▓██▓ ░ ░██░▒██   ██░▓██▒  ▐▌██▒
-▒ ▓███▀ ░░ ████▓▒░░██████▒░ ████▓▒░░██▓ ▒██▒   ░▒████▒▒██░   ▓██░▒ ▓███▀ ░░██▓ ▒██▒ ░ ██▒▓░▒██▒ ░  ░  ▒██▒ ░ ░██░░ ████▓▒░▒██░   ▓██░
-░ ░▒ ▒  ░░ ▒░▒░▒░ ░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░   ░░ ▒░ ░░ ▒░   ▒ ▒ ░ ░▒ ▒  ░░ ▒▓ ░▒▓░  ██▒▒▒ ▒▓▒░ ░  ░  ▒ ░░   ░▓  ░ ▒░▒░▒░ ░ ▒░   ▒ ▒
-  ░  ▒     ░ ▒ ▒░ ░ ░ ▒  ░  ░ ▒ ▒░   ░▒ ░ ▒░    ░ ░  ░░ ░░   ░ ▒░  ░  ▒     ░▒ ░ ▒░▓██ ░▒░ ░▒ ░         ░     ▒ ░  ░ ▒ ▒░ ░ ░░   ░ ▒░
-░        ░ ░ ░ ▒    ░ ░   ░ ░ ░ ▒    ░░   ░       ░      ░   ░ ░ ░          ░░   ░ ▒ ▒ ░░  ░░         ░       ▒ ░░ ░ ░ ▒     ░   ░ ░
-░ ░          ░ ░      ░  ░    ░ ░     ░           ░  ░         ░ ░ ░         ░     ░ ░                        ░      ░ ░           ░
-"""
-
 def generateColor():
     USED_COLOR_SCHEMES = []
     for n in ALPHA: # For each letter
-        color = (randint(0,255), randint(0,255), randint(0,255)) # generate a color
+        color = (randint(1,255), randint(1,255), randint(1,255)) # generate a color
         while(color in USED_COLOR_SCHEMES is False): # if the color has already been picked
             color = (randint(0,255), randint(0,255), randint(0,255)) # generate a new one and try again
         CTW[n] = color # and finally we are done
@@ -46,7 +34,7 @@ def encodeMessage(mess):
                 exit()
         return ENCODED_MESSAGE
 
-def decodeMessage(pathToFile):
+def decodeMessage(pathToFile, count):
     DECODED_MESSAGE = ""
     try:
         image = Image.open(pathToFile)
@@ -54,25 +42,44 @@ def decodeMessage(pathToFile):
         print("[!] Failed to open file {}".format(pathToFile))
         exit()
 
-    pixels = list(image.getdata())
-
-    for color in pixels:
+    x = 0
+    y = 0
+    width, height = image.size
+    pixels = image.load()
+    while(y != height):
         for value in CTW.keys():
-            if color == CTW[value]: # have we found the key which returns the value of 'color'
+            if (pixels[x,y] == CTW[value]): # have we found the key which returns the value of 'color'
                 DECODED_MESSAGE += value # if thats the case then add the key to our string
+        x += count
+        if (x == width):
+            x = 0
+            y += 1
+
     return DECODED_MESSAGE
 
-def writeToImage(ENCODED_MESSAGE, outputFileName, width, height):
+def writeToImage(ENCODED_MESSAGE, outputFileName, width, height, count, blanks):
     image = Image.new("RGB", (width, height), color=None) # Open the file
     pix = image.load() # Load it's contents to memory
     x = 0
     y = 0
     for n in ENCODED_MESSAGE:
         pix[x,y] = n # Adds the color code of n to the position of x and y in pix
-        x += 1
+        x += count
         if (x == width): # Have we reached the end of the line?
-            y += 1
+            y += count
             x = 0
+
+    if (blanks): # We will fill all the black pixels with a random color
+        x = 0
+        y = 0
+        while(x != width):
+            if (pix[x,y] == (0,0,0)): # Is the color black?
+                image.putpixel((x,y), choice(list(CTW.values()))) # Update it
+            y += 1
+            if (y == height):
+                y = 0
+                x += 1
+
     image.save(outputFileName) # Save the result
     print("[!] Image can be found in {}".format(outputFileName))
 
@@ -100,16 +107,12 @@ def loadEncoding(file): # Loads the given encoding into memory
         CTW[char] = eval(code) # Remove eval() and you will have hell
     openFile.close()
 
-def showCurrentEnc():
-    for n in CTW:
-        print("{}\t{}".format(n, CTW[n]))
-
 def updAlphabet(file):
     openFile = open(file, "r")
     for n in openFile:
         ALPHA.append(n[:-1])
 
-def fastEncrypt(message, width, height, readFile):
+def fastEncrypt(message, width, height, readFile, count, fillBlanks):
     generateColor()
 
     if (readFile): # We are gonna encrypt the contents of a file
@@ -119,12 +122,12 @@ def fastEncrypt(message, width, height, readFile):
             message.append(n)
 
     ENCODED_MESSAGE = encodeMessage(message)
-    writeToImage(ENCODED_MESSAGE, "result.png", width, height)
+    writeToImage(ENCODED_MESSAGE, "result.png", width, height, count, fillBlanks)
     storeEncoding()
 
-def fastDecrypt(imagePath, encPath, saveResult, runSys):
+def fastDecrypt(imagePath, encPath, saveResult, runSys, count):
     loadEncoding(encPath)
-    DECODED_MESSAGE = decodeMessage(imagePath)
+    DECODED_MESSAGE = decodeMessage(imagePath, count)
     if (saveResult): # We need to save it to the harddrive
         openFile = open("decodedMessage.txt", "w")
         openFile.write(DECODED_MESSAGE)
@@ -135,42 +138,6 @@ def fastDecrypt(imagePath, encPath, saveResult, runSys):
     else:
         print("Decoded message: {}".format(DECODED_MESSAGE))
 
-def main(ENCODED_FILE, store_enc, width, height):
-        run = True
-        VERSION = "1.2.0"
-        ENCODED_MESSAGE = []
-
-        if (len(CTW) == 0):
-            generateColor() # Generates the colors for each letter in the alphabet
-        if (store_enc):
-            storeEncoding()
-        print("{}\n\t\t\tVersion: {}".format(LOGO, VERSION))
-
-        while(run):
-            print("""
-1) Encode
-2) Decode
-3) Write image
-4) Show current encoding
-5) Quit
-""")
-            userInput = input(": ")
-            if userInput == "1":
-                ENCODED_MESSAGE = encodeMessage(input(">> Message to encode: "))
-            elif userInput == "2":
-                if ENCODED_FILE == None:
-                    DECODED_MESSAGE = decodeMessage(input(">> Path to file: "))
-                else:
-                    DECODED_MESSAGE = decodeMessage(ENCODED_FILE)
-                print("The decoded message is {}".format(DECODED_MESSAGE))
-            elif userInput == "3":
-                writeToImage(ENCODED_MESSAGE, "result.png", width, height)
-            elif userInput == "4":
-                showCurrentEnc()
-            elif userInput == "5":
-                run = False
-            else:
-                print("Unknown argument {}".format(userInput))
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -179,7 +146,9 @@ if __name__ == "__main__":
     parser.add_argument("--load_image", "-li", help="Loads an image into the program")
     parser.add_argument("--update_alpha", "-ua", help="Updates the internal alphabet with an additional amount of characters")
     parser.add_argument("--save_dec", "-sv", action="store_true", help="Saves the decrypted message to the harddrive, only usable with the --decrypt flag")
+    parser.add_argument("--count", "-c", help="The distance between each pixel of data, usable when encrypting and decrypting")
 
+    parser.add_argument("--fill_blanks", "-fb", action="store_true", help="Fills all the leftover pixels with garbage data")
     parser.add_argument("--sys", action="store_true", help="Tells the decrypter to run the decoded message, only usable with the --decrypt flag")
     parser.add_argument("--file", action="store_true", help="Marking that the contents of a file should be used to encrypt, only usable with the --encrypt flag")
     parser.add_argument("--width", help="Width of the generated picture")
@@ -192,9 +161,13 @@ if __name__ == "__main__":
     file = None
     height = 10
     width = 10
+    count = 1 # Default distance between each pod (pixel of data) is one
+    fill_blank = False
 
     if (args.store_enc):
         store_enc = True
+    if (args.fill_blanks):
+        fill_blank = True
     if (args.load_image):
         file = args.load_image
     if (args.load_enc):
@@ -205,13 +178,15 @@ if __name__ == "__main__":
         width = int(args.width)
     if (args.height):
         height = int(args.height)
+    if (args.count):
+        count = args.count
 
     if (args.encrypt):
-        fastEncrypt(args.encrypt, width, height, args.file)
+        fastEncrypt(args.encrypt, width, height, args.file, int(count), fill_blank)
     elif (args.decrypt):
         if (args.load_image and args.load_enc):
-            fastDecrypt(args.load_image, args.load_enc, args.save_dec, args.sys)
+            fastDecrypt(args.load_image, args.load_enc, args.save_dec, args.sys, int(count))
         else:
             print("main.py --decrypt -li <path/to/image> -le <path/to/enc> <optionalArgs>")
     else:
-        main(file, store_enc, width, height)
+        parser.print_help()
